17907161ab9567c00981601cda92e89d8ae5d116
wndjnky1 <wndjnky1@gmail.com>
2016-08-11 13:29:17 -0700

    Add files via upload

---
+++ Assignment9/form-parser2.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+Your name is: <?php echo $_POST['name']; ?><br />
+Your company name is: <?php echo $_POST['companyname']; ?><br />
+Your e-mail: <?php echo $_POST['email']; ?><br />
+Your phone is: <?php echo $_POST['phonenumber']; ?><br />
+
+    
+<br />
+Comments:<br />
+<?php echo $_POST['questions']; ?>
+</body>
+</html>
